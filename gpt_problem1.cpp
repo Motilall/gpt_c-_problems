8410c8b491a29d0f9fe353b8fa911b183e529124
Motilal <122986292+Motilall@users.noreply.github.com>
2024-04-12 20:35:12 +0530

    Add files via upload

---
+++ gpt_problem1.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,i;
+    cout<< "Enter till numbers you want to print from 1 \n";
+    cin>> a;
+
+    for(i = 1; i <= a; i++){
+        cout << i << endl;
+    }
+
+
+    return 0;
+}
